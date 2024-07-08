384930b9f49d560385fc81f1737a91a86dfe6468
yj.park <yj.park@okestro.com>
2024-07-08 10:23:51 +0900

    Add : Init Commit

---
+++ src/main/java/com/example/yjopsoperator/YjopsOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.yjopsoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjopsOperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjopsOperatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yjopsoperator/YjopsOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yjopsoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjopsOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
