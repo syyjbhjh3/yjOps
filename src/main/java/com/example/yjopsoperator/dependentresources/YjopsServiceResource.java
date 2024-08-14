7b7b575b6581f7b633f1fd801f5abae50c966c83
yj.park <yj.park@okestro.com>
2024-08-14 10:16:01 +0900

    Update : jenkins Service 작성

--- src/main/java/com/example/yjopsoperator/dependentresources/YjopsServiceResource.java
+++ src/main/java/com/example/yjopsoperator/dependentresources/YjopsServiceResource.java
@@ -2,7 +2,6 @@ package com.example.yjopsoperator.dependentresources;
 
 import com.example.yjopsoperator.customresources.Yjops;
 import io.fabric8.kubernetes.api.model.*;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
@@ -20,10 +19,31 @@ public class YjopsServiceResource extends CRUDKubernetesDependentResource<Servic
         final String yjopsName = yjopsMetadata.getName();
         final String mdName = yjops.getSpec().getMd();
 
+        /* Required Port */
+        final int httpPort = 80;
+        final int httpsPort = 443;
+        final int targetPort = 8080;
+        final int sshPort = 22;
+        /* Custom NodePort */
+        int jenkinsNodePort = 31010;
+        int gitlabNodePort = 31020;
+
         Service service = null;
 
         if (mdName.equals("jenkins")){
-
+            service = new ServiceBuilder()
+                    .editMetadata()
+                    .withName(mdName + "-service")
+                    .withNamespace(yjopsMetadata.getNamespace())
+                    .addToLabels("app", mdName)
+                    .endMetadata()
+                    .editSpec()
+                    .withType("NodePort")
+                    .addToSelector("app", mdName)
+                    .addToPorts(new ServicePortBuilder().withName("http").withPort(httpPort).withNodePort(jenkinsNodePort).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(targetPort).build()).build(),
+                                new ServicePortBuilder().withName("https").withPort(httpsPort).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(targetPort).build()).build())
+                    .endSpec()
+                    .build();
         } else if (mdName.equals("gitlab")) {
             service = new ServiceBuilder()
                     .editMetadata()
@@ -34,9 +54,9 @@ public class YjopsServiceResource extends CRUDKubernetesDependentResource<Servic
                     .editSpec()
                     .withType("NodePort")
                     .addToSelector("app", mdName)
-                    .addToPorts(new ServicePortBuilder().withName("http").withPort(80).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(80).build()).build(),
-                                new ServicePortBuilder().withName("httpd").withPort(443).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(443).build()).build(),
-                                new ServicePortBuilder().withName("ssh").withPort(22).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(22).build()).build())
+                    .addToPorts(new ServicePortBuilder().withName("http").withPort(httpPort).withNodePort(gitlabNodePort).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(httpPort).build()).build(),
+                                new ServicePortBuilder().withName("httpd").withPort(httpsPort).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(httpsPort).build()).build(),
+                                new ServicePortBuilder().withName("ssh").withPort(sshPort).withProtocol("TCP").withTargetPort(new IntOrStringBuilder().withValue(sshPort).build()).build())
                     .endSpec()
                     .build();
         }
